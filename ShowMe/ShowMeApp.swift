9130ff3d0d3341c0f7e13605c139ed48d430b188
degermon <degermon@gmail.com>
2021-07-25 19:08:26 +0300

    Initial Commit

---
+++ ShowMe/ShowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMeApp.swift
+//  ShowMe
+//
+//  Created by Daniel Šuškevič on 2021-07-25.
+//
+
+import SwiftUI
+
+@main
+struct ShowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
